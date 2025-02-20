d2b12f1dbb12e8a88b50e6c6776e2784799502cb
Frank Jia <jurong@claclaws.com>
2025-02-20 15:58:43 -0500

    jurong-first-commit

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <div
+        className="container mx-auto"
+      >
+        <h1
+          className="text-6xl text-center pt-8"
+        >This is Home Page</h1>
+        <div
+          className="flex items-center justify-center gap-4 mt-16"
+        >
+          <Link
+            href='/register'
+          >
+            <button
+              className="border-2 border-black rounded-md p-4 hover:bg-gray-200 active:bg-gray-400 w-[100px]"
+            >
+              Register
+            </button>
+          </Link>
+          <Link
+            href='/login'
+          >
+            <button
+              className="border-2 border-black rounded-md p-4 hover:bg-gray-200 active:bg-gray-400 w-[100px]"
+            >
+              Login
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
